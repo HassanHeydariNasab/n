2e57a9b48717cf921af20d58dbb8c9ebddcefa82
HassanHeydariNasab <hsn6@tuta.io>
2023-02-07 21:38:44 +0330

    Add flasher component

--- src/pages/components/index.js
+++ src/pages/components/index.js
@@ -1,2 +1,4 @@
 import { MyCounter } from "./counter.js";
+import { MyFlasher } from "./flasher.js";
 customElements.define("my-counter", MyCounter);
+customElements.define("my-flasher", MyFlasher);
